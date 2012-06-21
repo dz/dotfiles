d032ed2a5a6a8c3d09357dbbad63cf47919d948f
dz <david@nodnod.net>
2012-06-21 18:02:46 -0400

    sass support

--- emacsd/configs/css.el
+++ emacsd/configs/css.el
@@ -1,7 +1,9 @@
 ;; fix css mode
 (require 'less-css-mode)
-
 (setq cssm-indent-level 4)
 (setq cssm-newline-before-closing-bracket t)
 (setq cssm-indent-function #'cssm-c-style-indenter)
 (setq cssm-mirror-mode t)
+
+;; sass support
+(require 'scss-mode )
