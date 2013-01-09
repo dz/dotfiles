a961fdc796c5055425b8adf254fbf5ec123a20ad
dz <david@nodnod.net>
2013-01-09 04:18:45 -0500

    magit change

--- emacsd/configs/git.el
+++ emacsd/configs/git.el
@@ -1,9 +1,49 @@
 ;; git settings
 
-;; alias various vc commands to make more sense
-(defalias 'blame 'vc-annotate)
-
 ;; magit settings
 
 (require 'magit)
+(require 'magit-blame)
 
+;; make magit status go full-screen but remember previous window
+;; settings
+;; from: http://whattheemacsd.com/setup-magit.el-01.html
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+;; restore previously hidden windows
+(defadvice magit-quit-window (around magit-restore-screen activate)
+  (let ((current-mode major-mode))
+    ad-do-it
+    ;; we only want to jump to register when the last seen buffer
+    ;; was a magit-status buffer.
+    (when (eq 'magit-status-mode current-mode)
+      (jump-to-register :magit-fullscreen))))
+
+;; to play well with evil, i still want hljk in magit
+(evil-add-hjkl-bindings magit-branch-manager-mode-map 'emacs
+  "K" 'magit-discard-item
+  "L" 'magit-key-mode-popup-logging)
+(evil-add-hjkl-bindings magit-status-mode-map 'emacs
+  "K" 'magit-discard-item
+  "l" 'magit-key-mode-popup-logging
+  "h" 'magit-toggle-diff-refine-hunk)
+
+;; magit aliases
+(defalias 'blame 'magit-blame-mode)
+(defalias 'bl 'magit-blame-mode)
+(defalias 'status 'magit-status)
+(defalias 'st 'magit-status)
+
+;; config options
+
+;; use ido to look for branches
+(setq magit-completing-read-function 'magit-ido-completing-read)
+(setq magit-default-tracking-name-function 'magit-default-tracking-name-branch-only)
+(setq magit-status-buffer-switch-function 'switch-to-buffer)
+(setq magit-diff-refine-hunk t)
+(setq magit-rewrite-inclusive 'ask)
+(setq magit-save-some-buffers nil)
+(setq magit-process-popup-time 10)
