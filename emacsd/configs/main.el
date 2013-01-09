51e1c8232c046b9339ddfdf1dc8e8cab0e5f4316
dz <david@nodnod.net>
2013-01-09 06:19:26 -0500

    easier new line insertion

--- emacsd/configs/main.el
+++ emacsd/configs/main.el
@@ -289,3 +289,19 @@
         (delete-file filename)
         (kill-buffer buffer)
         (message "File '%s' successfully removed" filename)))))
+
+(defun open-line-below ()
+  (interactive)
+  (end-of-line)
+  (newline)
+  (indent-for-tab-command))
+
+(defun open-line-above ()
+  (interactive)
+  (beginning-of-line)
+  (newline)
+  (forward-line -1)
+  (indent-for-tab-command))
+
+(global-set-key (kbd "<C-return>") 'open-line-below)
+(global-set-key (kbd "<C-S-return>") 'open-line-above)
