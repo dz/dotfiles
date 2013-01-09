3f457b15e0b9073415f395daeee13018910e34f6
dz <david@nodnod.net>
2013-01-09 06:19:12 -0500

    toggle whitepsace in magit

--- emacsd/configs/git.el
+++ emacsd/configs/git.el
@@ -62,3 +62,21 @@
 (setq magit-diff-refine-hunk t)
 (setq magit-rewrite-inclusive 'ask)
 (setq magit-save-some-buffers nil)
+
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
